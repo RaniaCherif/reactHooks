580ac454ffc4785e0dca107e66fe21ebe0cacb3f
rania <contactraniacherif@gmail.com>
2023-04-06 14:26:01 +0100

    fixed: style css moviecard

--- reactHooks/src/components/movieCard/MovieCard.js
+++ reactHooks/src/components/movieCard/MovieCard.js
@@ -7,20 +7,24 @@ import "./movieCard.css";
 const RestoCard = ({ el }) => {
   return (
     <div className="card">
-      <Card style={{ width: "18rem", height: "26rem" }}>
+      <Card style={{ width: "18rem" }}>
         <Card.Img
+          className="img1"
           variant="top"
           src={el.img}
           style={{
             resizeMode: "cover",
             height: 200,
-            width: 285,
+            width: 400,
           }}
         />
-        <Card.Body>
-          <Card.Title className="title" style={{ color: "white" }}>{el.title}</Card.Title>
-          <Card.Text>{el.description}</Card.Text>
+        <Card.Body >
+          <Card.Title className="title" style={{ color: "white" }}>
+            {el.title}
+          </Card.Title>
+          <Card.Text className="text">{el.description}</Card.Text>
           <StarRatingComponent
+            className="views"
             name={"star"} /* name of the radio input, it is required */
             value={
               el.rate
@@ -30,7 +34,6 @@ const RestoCard = ({ el }) => {
             } /* is component available for editing, default `true` */
           />
         </Card.Body>
-        
       </Card>
     </div>
   );
